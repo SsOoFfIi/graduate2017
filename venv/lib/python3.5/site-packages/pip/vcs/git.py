2c89ce08f3b8a699da812da77264393d0ce1a7d5
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-23 07:11:17 +0300

    Discarding the master-slave language

--- venv/lib/python3.5/site-packages/pip/vcs/git.py
+++ venv/lib/python3.5/site-packages/pip/vcs/git.py
@@ -89,7 +89,7 @@ class Git(VersionControl):
     def update(self, dest, rev_options):
         # First fetch changes from the default remote
         self.run_command(['fetch', '-q'], cwd=dest)
-        # Then reset to wanted revision (maby even origin/master)
+        # Then reset to wanted revision (maby even origin/main)
         if rev_options:
             rev_options = self.check_rev_options(
                 rev_options[0], dest, rev_options,
@@ -104,7 +104,7 @@ class Git(VersionControl):
             rev_options = [rev]
             rev_display = ' (to %s)' % rev
         else:
-            rev_options = ['origin/master']
+            rev_options = ['origin/main']
             rev_display = ''
         if self.check_destination(dest, url, rev_options, rev_display):
             logger.info(
